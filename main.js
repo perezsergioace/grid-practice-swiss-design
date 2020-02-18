c3c919de28f2ac2a359d03eda440ec5967e8bf15
Sergio <sergio@Sergios-MBP.home>
2020-02-18 14:36:58 -0500

    added animation to the landing page

---
+++ main.js
@@ -0,0 +1,33 @@
+TweenMax.from(".hero-container", 1.5, {
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".item-e", 1.5, {
+    delay: .5,
+    opacity: 0,
+    x: "-100%",
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".item-c", 1.5, {
+    delay: 1,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".text-container", 1.5, {
+    delay: 1.5,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".item-f", 1.5, {
+    delay: 2,
+    opacity: 0,
+    y: "100%",
+    ease: Expo.easeInOut
+})
